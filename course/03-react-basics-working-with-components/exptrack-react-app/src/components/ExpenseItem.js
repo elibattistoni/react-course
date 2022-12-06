4f26072b9df69bd2c1037b5493a029cab55eb239
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-06 11:57:17 +0100

    normal javascript code in components

--- course/03-react-basics-working-with-components/exptrack-react-app/src/components/ExpenseItem.js
+++ course/03-react-basics-working-with-components/exptrack-react-app/src/components/ExpenseItem.js
@@ -28,10 +28,17 @@ import "./ExpenseItem.css"; // this simply tells the build process that this CSS
 function ExpenseItem(props) {
   //= props is an object that contains all the values of the attributes that we define when we call this custom component
   // the keys are the names of the attributes; the values are the relative values
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
