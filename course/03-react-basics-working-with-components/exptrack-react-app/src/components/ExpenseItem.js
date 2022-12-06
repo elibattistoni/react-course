1125b99be7651b7d272ef84e9a231dbe430d6fc8
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-06 11:03:53 +0100

    Style React Components

--- course/03-react-basics-working-with-components/exptrack-react-app/src/components/ExpenseItem.js
+++ course/03-react-basics-working-with-components/exptrack-react-app/src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 // IMPORTANT in React components there is one important rule
 /// regarding the JSX code that is returned: you must return only one root element per return statement (or per JSX code snippet)
 
+//# CSS styling
+import "./ExpenseItem.css"; // this simply tells the build process that this CSS file should be considered
+// NB in JSX you don't write class="" like in regular css but instead write className="" (because the wor class is a reserved work in JavaScript)
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>Date</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Title</h2>
-        <div>Amount</div>
+        <div className="expense-item__price">Amount</div>
       </div>
     </div>
   );
