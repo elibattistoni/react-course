be5c162d0c9a98245e957b954c23e37e835f5793
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-06 10:56:20 +0100

    More complex component

--- course/03-react-basics-working-with-components/exptrack-react-app/src/components/ExpenseItem.js
+++ course/03-react-basics-working-with-components/exptrack-react-app/src/components/ExpenseItem.js
@@ -1,5 +1,16 @@
+// IMPORTANT in React components there is one important rule
+/// regarding the JSX code that is returned: you must return only one root element per return statement (or per JSX code snippet)
+
 function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+        <div>Amount</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
