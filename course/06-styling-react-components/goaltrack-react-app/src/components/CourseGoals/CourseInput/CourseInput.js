f1d65f171e740369bbd150f95f11a3cf335c4c03
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-07 22:46:58 +0100

    dynamic classes with css modules

--- course/06-styling-react-components/goaltrack-react-app/src/components/CourseGoals/CourseInput/CourseInput.js
+++ course/06-styling-react-components/goaltrack-react-app/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,40 +1,41 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
-import styled from "styled-components";
+// import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
+// import styled from "styled-components";
 
 /*
 NB so far we have only defined one component per file, however, it is fine to have more components per file if the "other components" are used only in that file
 NB see CourseInput.css to understand how styles are translated
 */
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props) => (props.invalid ? "red" : "black")};
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.invalid ? "red" : "black")};
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-    background: 1px solid
-      ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+//     background: 1px solid
+//       ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 /*
 NB with the method with props.invalid we removed the following from the code above:
@@ -82,11 +83,19 @@ const CourseInput = (props) => {
       so that it changes dynamically the style NB it receives the props NB you can add props to your styled component!
       NB see line 24 border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
       */}
-      <FormControl invalid={!isValid}>
+      {/* with styled components: */}
+      {/* <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </FormControl> */}
       {/* </div> */}
+      {/* with css modules: add dynamic classes */}
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
