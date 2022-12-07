d47e7f23ab486a7d7dc79f69a2145de97f508d36
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-07 21:58:51 +0100

    styled components (package) for scoping css styles

--- course/06-styling-react-components/goaltrack-react-app/notes.md
+++ course/06-styling-react-components/goaltrack-react-app/notes.md
@@ -1,3 +1,10 @@
 # CSS Scoping
 So far we have used regular CSS with regular CSS selectors:
 even if we are importing a css file e.g. **CourseGoalList.css** in the **CourseGoulaList.js** and using the className "goal-list" for styling the list component, this css is not scoped: if on the webpage other components (that are not the CourseGoalList component) are styled with the "goal-list" className, they will be affected because this css class is not scoped to the CourseGoalList component (it would affect any element on the entire page (**downstream of the import**))
+
+NB by default the styles are not scoped: this is not necessarily a rpoblem because you can simply be careful about your selectors
+
+# 2 Approaches to scope styles
+1. Use a package named **Styled Components** https://styled-components.com/ : this package helps you build components that have certain styles attached to them, where the styles really only affect the components to which they are attached to, and not other components; install it with `npm install --save styled-components`
+
+JetBrains Mono https://www.jetbrains.com/lp/mono/#how-to-install https://vueschool.io/articles/vuejs-tutorials/how-to-install-jetbrains-mono-font-in-visual-studio-code/
--- course/06-styling-react-components/goaltrack-react-app/src/components/UI/Button/Button.js
+++ course/06-styling-react-components/goaltrack-react-app/src/components/UI/Button/Button.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import styled from "styled-components";
 
-import './Button.css';
+// NB the code below was written just becuase we wanted to have some default styling for a button
+// import "./Button.css";
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
+// NB there is nothing wrong about this approach but here we could also use the styled components package to achieve this in an easier way
+// what we store in the Button component is not a functional component
+/// styled components:
+// const Button = styled.button``; // this is called TAGGED TEMPLATE LITERAL (Javascript feature) https://wesbos.com/tagged-template-literals
+// NB .button is a method on the styled object (and styled is an object that we are importing from style components)
+// this is executed as a method behind the scenes, and what you pass among the `` is passed into this method in a special way
+//NB the button method will return a button component; NB the styled package has methods for all HTML elements
+// and you write the style between the `` and for pesudo selectors you add a &
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
+// this returns a button with these styles
+//NB the button that is returned also applies by default all the props that you might be passing to your Button component which in the end we export
+//IMPORTANT if you inspect the HTMl you will see that the button will have a strange class: this class is generated automatically (dynamically) by the Styled Components package and it ensures (since it has a weird name)
+/// that every class name is unique so that it cannot spill over to other components of the app and then it will add these classes as global CSS
 
 export default Button;
