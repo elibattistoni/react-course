c8f77ff84d2b8f29784708e2f9a4b77358c86b38
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-07 13:09:12 +0100

    refactoring + a different way of handling conditional content

--- course/05-lists-conditional-content/exptrack-react-app/src/components/Expenses/Expenses.js
+++ course/05-lists-conditional-content/exptrack-react-app/src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
@@ -16,19 +16,6 @@ const Expenses = (props) => {
     (expense) => expense.date.getFullYear().toString() === filteredYear
   );
 
-  let expensesContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-    // NB you should always add the key prop when rendering lists of items!!!
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -37,7 +24,7 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         />
         {/* conditional content */}
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
