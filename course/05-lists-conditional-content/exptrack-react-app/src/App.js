18c54e12205f28e39e6d67b7b21c04d83e261620
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-07 11:32:01 +0100

    Using STATEFUL LISTS: Render new expense in list of expenses

--- course/05-lists-conditional-content/exptrack-react-app/src/App.js
+++ course/05-lists-conditional-content/exptrack-react-app/src/App.js
@@ -1,32 +1,41 @@
+import { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    /// VERY IMPORTANT
+    // add new expense at the beginning and use the spread operator to get the values of the expenses array to form a new array
+    // setExpenses([expense, ...expenses]); // NB this is not correct because you want to update your state based on your previous state
+    //NB this is a case in which you need to update your state based on your previous state
+    //NB so you should use a special function form, so you pass a function to the state updating function, and that function will automatically receive the latest state snapshot
+    setExpenses((previousExpenses) => {
+      return [expense, ...previousExpenses];
+    });
   };
 
   return (
