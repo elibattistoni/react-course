8f96d3b118e802b9a398f0eb13294d32839576aa
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-07 22:41:40 +0100

    css modules for the button component

--- course/06-styling-react-components/goaltrack-react-app/notes.md
+++ course/06-styling-react-components/goaltrack-react-app/notes.md
@@ -6,5 +6,6 @@ NB by default the styles are not scoped: this is not necessarily a rpoblem becau
 
 # 2 Approaches to scope styles
 1. Use a package named **Styled Components** https://styled-components.com/ : this package helps you build components that have certain styles attached to them, where the styles really only affect the components to which they are attached to, and not other components; install it with `npm install --save styled-components`
+2. **CSS Modules** CSS Modules is a feature which is only available in projects that are configured to support it, because it needs a code transformation that needs to be done before your code runs in the browser. Now, the good thing is the react projects created with create react app which we used are already configured to support CSS Modules. (reference: https://create-react-app.dev/docs/adding-a-css-modules-stylesheet/)
 
 JetBrains Mono https://www.jetbrains.com/lp/mono/#how-to-install https://vueschool.io/articles/vuejs-tutorials/how-to-install-jetbrains-mono-font-in-visual-studio-code/
--- course/06-styling-react-components/goaltrack-react-app/src/components/UI/Button/Button.js
+++ course/06-styling-react-components/goaltrack-react-app/src/components/UI/Button/Button.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
 // NB the code below was written just becuase we wanted to have some default styling for a button
 // import "./Button.css";
@@ -17,35 +17,53 @@ import styled from "styled-components";
 // this is executed as a method behind the scenes, and what you pass among the `` is passed into this method in a special way
 //NB the button method will return a button component; NB the styled package has methods for all HTML elements
 // and you write the style between the `` and for pesudo selectors you add a &
-const Button = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// NB commenting Button styled component because we are doing it noe with CSS Modules (which allows to keep separation between css and Javascript)
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  @media (min-width: 768px) {
-    width: auto;
-  }
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
 
-  &:focus {
-    outline: none;
-  }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 // NB get rid of the selectors (see Button.css to understand)
 // this returns a button with these styles
 //NB the button that is returned also applies by default all the props that you might be passing to your Button component which in the end we export
 //IMPORTANT if you inspect the HTMl you will see that the button will have a strange class: this class is generated automatically (dynamically) by the Styled Components package and it ensures (since it has a weird name)
 /// that every class name is unique so that it cannot spill over to other components of the app and then it will add these classes as global CSS
 
+import React from "react";
+import styles from "./Button.module.css"; //NB particular import: this is how you should import css if you want to use css modules, and you need to rename your file with Button.module.css
+/// this is basically a signal to the underlying compilation process to transform the code so that CSS Modules work
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {/* NB IMPORTANT see className={styles.button} */}
+      {props.children}
+    </button>
+  );
+};
+
+/*
+NB what the build process does under the hood is that it takes those CSS classes and CSS files and basically changes those class names to be unique
+/ this CSS module concept ensures that the CSS styles that we set up in the CSS file are scoped to the component we import this file into.
+/ in the CSS we need to work with classes because we then access those classes as properties on the imported styles object
+*/
 export default Button;
