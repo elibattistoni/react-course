2c3ca090e12a60607ca331168360a7c114cfd394
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-08 09:24:05 +0100

    Managing the user input state

--- course/08-practice-project/user-adder-react-app/src/components/Users/AddUser.js
+++ course/08-practice-project/user-adder-react-app/src/components/Users/AddUser.js
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  //============================================================================
+  //# Retrieve user input
+  //============================================================================
+  /*
+  # to collect the values of the form, we can use a technique called State Management in React,
+  # to update the state with every keystroke (i.e. "onChange") and hence save what the user enters in this state variable
+  */
+  // the starting input will be no input, i.e. empty string
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  //# state change handler functions
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  //============================================================================
+  //# Handler function for form submission
+  //============================================================================
   // handler function that should be executed when the for is submitted (i.e. onSubmit)
   const addUserHandler = (event) => {
     event.preventDefault();
     console.log("form submitted!");
+    console.log(enteredUsername, enteredAge);
   };
 
   return (
@@ -15,9 +39,9 @@ const AddUser = (props) => {
       {/* therefore when defining the Card component in Card.js you have to make sure that it can receive classes from props.className */}
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
+        <input type="text" id="username" onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input type="number" id="age" />
+        <input type="number" id="age" onChange={ageChangeHandler} />
         <Button type="submit">Add User</Button>
         {/* <button type="submit">Add User</button> */}
       </form>
