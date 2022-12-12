4c6401f17a7d23b520e621f2d8857b5e7b133f86
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-12 18:21:55 +0100

    FOA the useEffect hook for showing animations on a button

--- course/11-practice-project-food-order-app/react-app-food-order/src/components/Layout/HeaderCartButton.js
+++ course/11-practice-project-food-order-app/react-app-food-order/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
@@ -11,13 +11,37 @@ the CartIcon component simly contains an SVG
 */
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
   const numberOfCartItems = cartCtx.items.reduce((accumulator, item) => {
     return accumulator + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    // bump effect only if there are n > 0 items in the cart
+    if (cartCtx.items.length === 0) return;
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    // 300 because it is the duration of the animation (cfr css)
+    // NB even if the button is not going to be removed, it is a best practice
+    // NB to clear any timers or side eddects that might be ongoing because you started them in useEffect
+    // NB IMPORTANT this timer could be set again before it expires, therefore you MUST clear it!
+    // so return the cleanup function:
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
+
   return (
-    <button className={classes.button} onClick={props.onClickHandler}>
+    <button className={btnClasses} onClick={props.onClickHandler}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
