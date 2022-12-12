5be6592adddea1a231574175457492916f56420b
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-12 10:04:42 +0100

    FOA - create Header component

---
+++ course/11-practice-project-food-order-app/react-app-food-order/src/components/Layout/Header.js
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
+
+/*
+The Header component returns a Fragment in order to wrap the two children siblings.
+It contains a header built-in component, with the title and the button for the cart,
+and a div builtin component which contains an image.
+*/
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
