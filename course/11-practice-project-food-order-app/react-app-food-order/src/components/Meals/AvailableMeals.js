d33fa8833e7897aaaed9eabb813d84d5fbc6fbb0
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-12 11:01:58 +0100

    Add MealItem to AvailableMeals

--- course/11-practice-project-food-order-app/react-app-food-order/src/components/Meals/AvailableMeals.js
+++ course/11-practice-project-food-order-app/react-app-food-order/src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 /*
 this component returns the actual list of meals
@@ -35,7 +36,14 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
     <section className={classes.meals}>
       <Card>
