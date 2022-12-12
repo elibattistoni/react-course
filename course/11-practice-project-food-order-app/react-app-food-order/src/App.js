978fa0adc3404b4f79ce4863b267ec2c50707451
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-12 14:32:45 +0100

    Create and render Modal component via React Portal

--- course/11-practice-project-food-order-app/react-app-food-order/src/App.js
+++ course/11-practice-project-food-order-app/react-app-food-order/src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
