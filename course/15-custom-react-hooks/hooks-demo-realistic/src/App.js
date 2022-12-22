8c7f84fbdc5473e0047fec8e1593b14fe7f63532
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-22 11:00:17 +0100

    Adding custom Hook for get and post requests

--- course/15-custom-react-hooks/hooks-demo-realistic/src/App.js
+++ course/15-custom-react-hooks/hooks-demo-realistic/src/App.js
@@ -1,19 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
+  //- refactoring with useHttp hook
+  /*
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   const fetchTasks = async (taskText) => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+          "https://tasks-react-app-8d171-default-rtdb.europe-west1.firebasedatabase.app/tasks.json"
       );
 
       if (!response.ok) {
@@ -34,10 +37,27 @@ function App() {
     }
     setIsLoading(false);
   };
-
   useEffect(() => {
     fetchTasks();
   }, []);
+  */
+
+  const transformTasks = useCallback((data) => {
+    const loadedTasks = [];
+    for (const taskKey in data) {
+      loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+    }
+    setTasks(loadedTasks);
+  }, []);
+
+  // call custom hook
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
+
+  useEffect(() => {
+    fetchTasks({
+      url: "https://tasks-react-app-8d171-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+    });
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
@@ -45,7 +65,7 @@ function App() {
 
   return (
     <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
+      {/*<NewTask onAddTask={taskAddHandler} />*/}
       <Tasks
         items={tasks}
         loading={isLoading}
