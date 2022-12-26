6025a7a0be704b46542b43e784b6a9e5bdd103fd
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-26 17:11:15 +0100

    disadvantages of react-redux for managing state and introducting the solution: redux toolkit

--- course/18-redux/01-redux-react-app-basics/src/store/index.js
+++ course/18-redux/01-redux-react-app-basics/src/store/index.js
@@ -40,3 +40,13 @@ const counterReducer = (currentState = initialState, action) => {
 const store = createStore(counterReducer);
 
 export default store;
+
+/*
+% POTENTIAL PROBLEMS WITH REACT-REDUX
+| 1) typos in action names
+| 2) really really long redux files (because you have to always return all the properties of the state object)
+| 3) easy to mess up and change the currentState when you have a really complex state object and a lot of data
+
+| in the past there were solutions to these problems, like using named exports for the action types and creating multiple and smaller redux files
+| but we don't need this anymore because we have the Redux Toolkit, which is a library that makes things really much easier
+*/
