d669e0bcd861e902524bc6a736ea90de17c5c88e
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-26 17:03:46 +0100

    react redux - working correctly with state

--- course/18-redux/01-redux-react-app-basics/src/store/index.js
+++ course/18-redux/01-redux-react-app-basics/src/store/index.js
@@ -2,9 +2,17 @@ import { createStore } from "redux";
 
 const initialState = { counter: 0, amount: 1, showCounter: true };
 
-//NB CREATE REDUCER FUNCTION
+//% CREATE REDUCER FUNCTION
 const counterReducer = (currentState = initialState, action) => {
+  //§ WORKING CORRECTLY WITH REDUX STATE
+  //NB the objects that we return in the reducer willNOT be merged with the existing state: they overwrite the existing state!
+  //NB so when we update a single piece opf state (i.e. a single property of the state) we must always set also the other pieces of state!
+  //NB you MUST overwrite it: it is the best practice and an important rule when using Redux
+  //NB IMPORTANT always overwrite it and return a brand new object
+
   if (action.type === "increment")
+    //NB it is extremely IMPORTANT that you never mutate the currentState!
+    //NB do not do this: currentState.counter++ and then return currentState
     return {
       counter: currentState.counter + currentState.amount,
       amount: currentState.amount,
@@ -28,7 +36,7 @@ const counterReducer = (currentState = initialState, action) => {
   return currentState;
 };
 
-//NB CREATE STORE
+//% CREATE STORE
 const store = createStore(counterReducer);
 
 export default store;
