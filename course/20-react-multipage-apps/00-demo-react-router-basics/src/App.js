582c1e5f8c165c591e45414179abd8c06a05785d
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-28 09:26:05 +0100

    React Router basics

--- course/20-react-multipage-apps/00-demo-react-router-basics/src/App.js
+++ course/20-react-multipage-apps/00-demo-react-router-basics/src/App.js
@@ -1,7 +1,30 @@
+//% for routing you need to do this
+import { Route } from "react-router-dom";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+//| Route is a component that allows us to define a certain path and the React
+//| component that should be loaded when that path becomes active in the URL
+
+//% create the folder "pages" or "screens" instead of "components" (best practice)
+//% because we do not use these components (Welcome and Products) like all the other components
+//% because they are use in a special way, in the sense that we use them as routes (NB but they are regular React components!!)
+//% i.e. components that we load through the router
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      {/*
+      //% define different routes
+      //NB the Route component makes sure that the Welcome component is only
+      //NB displayed on the screen if the URL path is /welcome, otherwise it will not be displayed
+      */}
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
 }
--- course/20-react-multipage-apps/00-demo-react-router-basics/src/index.js
+++ course/20-react-multipage-apps/00-demo-react-router-basics/src/index.js
@@ -1,7 +1,17 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+//% for routing you need to do this
+import { BrowserRouter } from "react-router-dom";
+//| this is another component provided by react-router-dom
+//| and you need to wrap the App component with this BrowserRouter component
+//| --> this will activate the React Routert and unlock the React Router features (like defining routes)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
