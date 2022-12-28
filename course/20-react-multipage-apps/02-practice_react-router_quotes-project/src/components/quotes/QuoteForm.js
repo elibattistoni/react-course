2956c6c57f5f9537d9cf160f80159a2dc8a9b94a
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-28 17:15:55 +0100

    add Prompt component to avoid leaving the page before submitting form

--- course/20-react-multipage-apps/02-practice_react-router_quotes-project/src/components/quotes/QuoteForm.js
+++ course/20-react-multipage-apps/02-practice_react-router_quotes-project/src/components/quotes/QuoteForm.js
@@ -1,10 +1,19 @@
-import { useRef } from 'react';
+import { Fragment, useRef, useState } from "react";
+import { Prompt } from "react-router-dom";
+//| Prompt is a component that we can render, and this component will automatically watch if we navigate away
+//| and if a certain condition is met, it will show a warning before it allows us to leave
 
-import Card from '../UI/Card';
-import LoadingSpinner from '../UI/LoadingSpinner';
-import classes from './QuoteForm.module.css';
+import Card from "../UI/Card";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./QuoteForm.module.css";
+
+//% PROMPT: "DO YOU REALLY WANT TO NAVIGATE AWAY?" if accidentally the user clicks
+//% on back and tries to leave the page after they have started entereing something in the form
+//| 1. determine when the user starts working on the form: when the form gains focus
+//| 2. we want to show a warning to the user if they try to leave the page after starting to work on the form
 
 const QuoteForm = (props) => {
+  const [isFormFocused, setIsFormFocused] = useState(false);
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -19,28 +28,58 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+  const formFocusHandler = () => {
+    //| this function is needed to determine when the user starts working on the form
+    console.log("FOCUS!");
+    setIsFormFocused(true);
+  };
 
-        <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
+  const formCompletedHandler = () => {
+    setIsFormFocused(false);
+  };
+
+  return (
+    <Fragment>
+      {/*
+        Prompt needs 2 props:
+        1) when: true or false --> to set whether this prompt should be shown if the user changes the URL or not
+        2) message: a function that returns a message (an the input argument is the location i.e. the page you want to go to)
+      */}
+      <Prompt
+        when={isFormFocused}
+        message={(location) =>
+          "Are you sure you want to leave? All the entered data will be lost"
+        }
+      />
+
+      <Card>
+        <form
+          onFocus={formFocusHandler}
+          className={classes.form}
+          onSubmit={submitFormHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+
+          <div className={classes.control}>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button className="btn" onClick={formCompletedHandler}>
+              Add Quote
+            </button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
   );
 };
 
--- course/20-react-multipage-apps/02-practice_react-router_quotes-project/src/pages/NewQuote.js
+++ course/20-react-multipage-apps/02-practice_react-router_quotes-project/src/pages/NewQuote.js
@@ -1,14 +1,15 @@
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
-//% here we want to add PROGRAMMATIC (IMPERATIVE) NAVIGATION: after the form submission,
-//% we want to redirect the user somewhere else
-//% we want to trigger a navigaiton action and navigate the user away programmatically
-//% so it is not a link that the user clicks to navigate away, but it is some action
-//% triggered by our code when some other action (sending data to a server) finished
-//% and we want to trigger the navigation action from inside the NewQuote page component
-//% how do we tell react router that we want to switch pages? with a react router hook: the useHistory hook
-//% it is named like this because it allows us to change browser history (i.e. the history of the pages we visited)
+//% PROGRAMMATIC or IMPERATIVE NAVIGATION
+//| here we want to add PROGRAMMATIC (IMPERATIVE) NAVIGATION: after the form submission,
+//| we want to redirect the user somewhere else
+//| we want to trigger a navigaiton action and navigate the user away programmatically
+//| so it is not a link that the user clicks to navigate away, but it is some action
+//| triggered by our code when some other action (sending data to a server) finished
+//| and we want to trigger the navigation action from inside the NewQuote page component
+//| how do we tell react router that we want to switch pages? with a react router hook: the useHistory hook
+//| it is named like this because it allows us to change browser history (i.e. the history of the pages we visited)
 
 const NewQuote = (props) => {
   //| redirect after submitting the form
