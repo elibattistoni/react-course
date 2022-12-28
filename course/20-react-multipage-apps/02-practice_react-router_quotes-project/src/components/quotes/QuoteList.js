83eed9f548a75e9b9da674f231d4e6e1ce9179ea
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-28 17:42:28 +0100

    sorting functionality! IMPORTANT

--- course/20-react-multipage-apps/02-practice_react-router_quotes-project/src/components/quotes/QuoteList.js
+++ course/20-react-multipage-apps/02-practice_react-router_quotes-project/src/components/quotes/QuoteList.js
@@ -1,13 +1,58 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+//% useHistory
+//| this hook allows us to change the page history, and therefore the URL
+//| and we can use this hook (and the history object it returns) to add QUERY PARAMETERS to the currently loaded page
+//| --> useHistory gives us access to the history object, which allows us to change and manage the URL
+//% useLocation
+//| this hook allows us to read query parameter values
+//| --> useLocation gives us access to a location object which holds information about the currently loaded URL
+
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
+  //NB this component function will be re-executed whenever we change the query parameters by clicking on the button
+
+  //% sorting functionality (that remains in the url with ?)
+  const history = useHistory();
+
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search); //NB this is a constructor function built-in in the browser
+  const isSortingAscending = queryParams.get("sort") === "asc";
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const changeSortingHandler = () => {
+    //| 1. update the URL with ?ascending so that we can share the URL with sotred data
+    //| remember that here we user ?sort= .. but sort is not mandatory, it can be another value
+    //| sort will be the key that holds our sorting information
+    //| and we will set a dynamic value that we derive from our current state of order
+    history.push(`/quotes?sort=${isSortingAscending ? "desc" : "asc"}`); //NB this will become dinamic
+    //| 2. we want to read the query parameter value and act accordingly (i.e. change the sorting, change the button label,...-)
+    //| read the query parameter values with useLocation() hook
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
