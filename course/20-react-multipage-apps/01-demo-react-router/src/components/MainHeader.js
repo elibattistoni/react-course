97cf0e09bad3da16b8bd0a0b3886b58ca2d060b2
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-12-28 10:05:14 +0100

    react-router better navigation

--- course/20-react-multipage-apps/01-demo-react-router/src/components/MainHeader.js
+++ course/20-react-multipage-apps/01-demo-react-router/src/components/MainHeader.js
@@ -1,11 +1,21 @@
 //% import the Link component from rect-router-dom
-//% it allows us to create a link for a FAKE NAVIGATION so that we do NOT SEND REQUESTS, the page is not reloaded, and we do not lose the state!!
-//% so you should ALWAYS use this Link component instead of the standard <a></a> tag!!!
-import { Link } from "react-router-dom";
+//NB Link Component:
+//- it allows us to create a link for a FAKE NAVIGATION so that we do NOT SEND REQUESTS, the page is not reloaded, and we do not lose the state!!
+//- so you should ALWAYS use this Link component instead of the standard <a></a> tag!!!
+
+//% alternatively, you can use the NavLink component, which has some nice extra features (e.g. making it clear which page you are on)
+//NB NavLink component
+//- it has the same features of the Link component, but it also sets a CSS class
+//- on the active anchor item (we just need to tell NavLink which class to add, through the activeClassName prop)
+//NB to have this feature, you also have to define in the css the ".header a.active" style
+import { Link, NavLink } from "react-router-dom";
+
+import classes from "./MainHeader.module.css";
 
 const MainHeader = () => {
-  return (
-    <header>
+  //% this is with the Link component of react-router-dom
+  const contentHeaderLink = (
+    <header className={classes.header}>
       <nav>
         <ul>
           <li>
@@ -18,6 +28,27 @@ const MainHeader = () => {
       </nav>
     </header>
   );
+
+  //% this is with the NavLink component of react-router-dom
+  const contentHeaderNavLink = (
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/welcome" activeClassName={classes.active}>
+              Welcome
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/products" activeClassName={classes.active}>
+              Products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+  return contentHeaderNavLink;
 };
 
 export default MainHeader;
