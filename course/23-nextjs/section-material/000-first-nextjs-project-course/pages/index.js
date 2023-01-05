285a91af4eb24c5a88642453b48f838fb566f289
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2023-01-05 14:37:29 +0100

    prepare code

--- course/23-nextjs/section-material/000-first-nextjs-project-course/pages/index.js
+++
@@ -1,9 +0,0 @@
-//| this will be our root page
-//| so if a request reaches our domain / nothing, then just index.js will be loaded
-//| the "index" name is a special name, and it is served whenever we have a request to just /
-
-const HomePage = (props) => {
-  return <h1>The Home Page</h1>;
-};
-
-export default HomePage;
